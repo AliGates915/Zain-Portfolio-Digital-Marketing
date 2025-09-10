dae4cd3e57eba8f1ef98b553fad662ad70c1ea81
Ali Gates <hacktech877@gmail.com>
2025-09-10 15:50:31 +0500

    check

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -99,8 +99,8 @@ export default function Hero() {
         <motion.div whileHover={{ y: -14 }}>
           <img
             className="w-52 sm:w-72"
-            // src={assests.man}
-            alt="Gandu"
+            src={assests.man}
+            alt="Hero"
           />
         </motion.div>
       </motion.div>
