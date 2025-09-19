bde05ced977ba1f27f2fada64e9805fc8dc26f7e
Ali Gates <hacktech877@gmail.com>
2025-09-19 09:46:47 +0500

    pooo

--- src/constants/assets.js
+++ src/constants/assets.js
@@ -1,6 +1,6 @@
 // ✅ Correct way
 export const assests = {
-  man: "/assests/hero.jpg",
+  man: "/assests/hero.png",
   instagram: "https://instagram.com",
   linkedin: "https://linkedin.com",
   whatsapp: "https://wa.me/1234567890",
